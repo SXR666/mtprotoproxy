35f7e521c132e6d0ec37c7a19a423a5a2b90f3fb
SXR666 <43741453+SXR666@users.noreply.github.com>
2021-03-21 18:18:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ PORT = 443
 
 # name -> secret (32 hex chars)
 USERS = {
-    "tg":  "00000000000000000000000000000001",
+    "tg":  "012334sxr666456789fanqiang666def",
     # "tg2": "0123456789abcdef0123456789abcdef",
 }
 
@@ -21,7 +21,7 @@ MODES = {
 
 # The domain for TLS mode, bad clients are proxied there
 # Use random existing domain, proxy checks it on start
-# TLS_DOMAIN = "www.google.com"
+TLS_DOMAIN = "www.cloudflare.com"
 
 # Tag for advertising, obtainable from @MTProxybot
-# AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
+AD_TAG = "284d05e57156ec0900537153378f1a46"
